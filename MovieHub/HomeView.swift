1f0c687817fa5a988f83f736f67b21dabfc4fd4c
adityaslyf <aditya.varshneymail@gmail.com>
2026-02-13 00:50:16 +0530

    feat: build homeview

--- MovieHub/HomeView.swift
+++ MovieHub/HomeView.swift
@@ -18,11 +18,19 @@ struct HomeView: View {
                         image
                             .resizable()
                             .scaledToFit()
+                            .overlay {
+                                LinearGradient(
+                                    stops: [Gradient.Stop(color: .clear, location: 0.8),
+                                            Gradient.Stop(color: .gradient, location: 1)
+                                           ],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                            }
                     } placeholder: {
                         ProgressView()
                     }
                     .frame(width: geo.size.width, height: geo.size.height * 0.85) // 85% of screen
-                
+                    
                     HStack {
                         Button{
                         } label: {
